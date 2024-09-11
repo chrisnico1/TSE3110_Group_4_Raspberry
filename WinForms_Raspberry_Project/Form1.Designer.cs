03c0ad25e1e4669544aa25b7e1a692286a8e97f2
Anthony <anthony.andreassen@outlook.com>
2024-09-11 11:15:28 +0200

    removed progress bar

--- WinForms_Raspberry_Project/Form1.Designer.cs
+++ WinForms_Raspberry_Project/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             btnAcknowledge = new Button();
             dataGridView1 = new DataGridView();
-            progressBar1 = new ProgressBar();
             label1 = new Label();
             label2 = new Label();
             btnClearAlarm = new Button();
@@ -43,9 +42,10 @@
             // 
             // btnAcknowledge
             // 
-            btnAcknowledge.Location = new Point(51, 186);
+            btnAcknowledge.Location = new Point(95, 397);
+            btnAcknowledge.Margin = new Padding(6, 6, 6, 6);
             btnAcknowledge.Name = "btnAcknowledge";
-            btnAcknowledge.Size = new Size(109, 23);
+            btnAcknowledge.Size = new Size(202, 49);
             btnAcknowledge.TabIndex = 0;
             btnAcknowledge.Text = "Acknowledge";
             btnAcknowledge.UseVisualStyleBackColor = true;
@@ -54,44 +54,41 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(51, 30);
+            dataGridView1.Location = new Point(95, 64);
+            dataGridView1.Margin = new Padding(6, 6, 6, 6);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 82;
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(698, 150);
+            dataGridView1.Size = new Size(1296, 320);
             dataGridView1.TabIndex = 1;
-            // 
-            // progressBar1
-            // 
-            progressBar1.Location = new Point(649, 186);
-            progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(100, 23);
-            progressBar1.TabIndex = 2;
-            progressBar1.Value = 50;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(51, 306);
+            label1.Location = new Point(95, 653);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(65, 15);
+            label1.Size = new Size(131, 32);
             label1.TabIndex = 4;
             label1.Text = "Light Satus";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(51, 12);
+            label2.Location = new Point(95, 26);
+            label2.Margin = new Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new Size(55, 15);
+            label2.Size = new Size(112, 32);
             label2.TabIndex = 5;
             label2.Text = "Database";
             // 
             // btnClearAlarm
             // 
-            btnClearAlarm.Location = new Point(166, 186);
+            btnClearAlarm.Location = new Point(308, 397);
+            btnClearAlarm.Margin = new Padding(6, 6, 6, 6);
             btnClearAlarm.Name = "btnClearAlarm";
-            btnClearAlarm.Size = new Size(90, 23);
+            btnClearAlarm.Size = new Size(167, 49);
             btnClearAlarm.TabIndex = 6;
             btnClearAlarm.Text = "Clear Alarm";
             btnClearAlarm.UseVisualStyleBackColor = true;
@@ -99,9 +96,10 @@
             // btnDiode
             // 
             btnDiode.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDiode.Location = new Point(51, 324);
+            btnDiode.Location = new Point(95, 691);
+            btnDiode.Margin = new Padding(6, 6, 6, 6);
             btnDiode.Name = "btnDiode";
-            btnDiode.Size = new Size(55, 56);
+            btnDiode.Size = new Size(102, 119);
             btnDiode.TabIndex = 7;
             btnDiode.Text = "💡";
             btnDiode.UseVisualStyleBackColor = true;
@@ -109,35 +107,38 @@
             // textBox1
             // 
             textBox1.BackColor = SystemColors.ScrollBar;
-            textBox1.Location = new Point(568, 236);
+            textBox1.Location = new Point(1055, 503);
+            textBox1.Margin = new Padding(6, 6, 6, 6);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(26, 144);
+            textBox1.Size = new Size(45, 303);
             textBox1.TabIndex = 8;
             // 
             // textBox2
             // 
             textBox2.BackColor = SystemColors.ControlDarkDark;
-            textBox2.Location = new Point(462, 236);
+            textBox2.Location = new Point(858, 503);
+            textBox2.Margin = new Padding(6, 6, 6, 6);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 144);
+            textBox2.Size = new Size(182, 303);
             textBox2.TabIndex = 9;
             // 
             // lblDoorStatus
             // 
             lblDoorStatus.AutoSize = true;
-            lblDoorStatus.Location = new Point(485, 218);
+            lblDoorStatus.Location = new Point(901, 465);
+            lblDoorStatus.Margin = new Padding(6, 0, 6, 0);
             lblDoorStatus.Name = "lblDoorStatus";
-            lblDoorStatus.Size = new Size(68, 15);
+            lblDoorStatus.Size = new Size(138, 32);
             lblDoorStatus.TabIndex = 10;
             lblDoorStatus.Text = "Door Status";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 440);
+            ClientSize = new Size(1486, 939);
             Controls.Add(lblDoorStatus);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -145,9 +146,9 @@
             Controls.Add(btnClearAlarm);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(progressBar1);
             Controls.Add(dataGridView1);
             Controls.Add(btnAcknowledge);
+            Margin = new Padding(6, 6, 6, 6);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -160,7 +161,6 @@
 
         private Button btnAcknowledge;
         private DataGridView dataGridView1;
-        private ProgressBar progressBar1;
         private Label label1;
         private Label label2;
         private Button btnClearAlarm;
--- WinForms_Raspberry_Project/obj/Debug/net7.0-windows/WinFormsTestOne.AssemblyInfo.cs
+++ WinForms_Raspberry_Project/obj/Debug/net7.0-windows/WinFormsTestOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsTestOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4083dfced34b7c9dd95ecb8f6561aa7324d0a55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsTestOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsTestOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
